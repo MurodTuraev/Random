ab33e7aa3f76f9a4952f29a55007bc3b062df7bb
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-01-27 18:54:29 +0500

    random

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print('hello')
+import requests
+headers = {'X-Api-Key': '0058a1e7b9a648259e3e52e129f5902c'}
+url = 'https://randommer.io/api/Card'
+r = requests.get(url, headers=headers)
+print(r.url)
