2f81627cd546b08429e812af5242e29a5d157901
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-01-27 18:55:59 +0500

    data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
 headers = {'X-Api-Key': '0058a1e7b9a648259e3e52e129f5902c'}
 url = 'https://randommer.io/api/Card'
-r = requests.get(url, headers=headers)
-print(r.url)
+r = requests.get(url, headers=headers).json()
+print(r)
